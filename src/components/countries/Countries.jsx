e65f3e26a3d2a85a5b17751f13e9f14814b01db0
NazmusSakib <n.sakib.sad@gmail.com>
2024-09-02 01:40:16 +0600

    added bg

--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -1,5 +1,6 @@
 import {useState, useEffect } from "react";
 import Country from "../country/Country";
+import './Countries.css'
 const Countries = () => {
   const  [countries, setCountries] = useState([]);
 
@@ -14,9 +15,14 @@ const Countries = () => {
     
     <div>
       <h2>Countries: {countries.length} </h2>
-      {
-        countries.map(country => <Country key={country.cca3} country={country} ></Country>)
-      }
+      <div className="countries-container">
+        {
+          countries.map(country => <Country 
+            key={country.cca3} 
+            country={country} ></Country>)
+        }
+      </div>
+      
     </div>
   );
 };
--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,10 +1,21 @@
+import { useState } from 'react';
 import './Country.css'
 const Country = ({country}) => {
-  const  {name, flags} = country;
+  const  {name, flags, population, area, cca3} = country;
+
+  const [added, setAdded] = useState(false);
+  const isAdded = () => {
+    setAdded(!added);
+  }
+
   return (
-    <div className='box'>
+    <div className={`box ${added && 'added'}`}>
       <h2>Name: {name?.common} </h2>
       <img src={flags.png} alt="Loading Image Please Wait A Few Seconds" />
+      <h2>Population: {population}</h2>
+      <h2>Area: {area} square km </h2>
+      <h2>Country Code: {cca3} </h2>
+      <button onClick={isAdded} > {added ? "Added" : "Add to visit list"} </button>
     </div>
   );
 };
